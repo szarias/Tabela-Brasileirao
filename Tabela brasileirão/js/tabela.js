48522b2ec7a701a34d41291536780224d7c9b027
ariasz <129909951+szarias@users.noreply.github.com>
2023-07-10 21:53:27 -0300

    Tabela do brasileirão - Simulação site
    att.2
    funci

--- Tabela brasileirão/js/tabela.js
+++ Tabela brasileirão/js/tabela.js
@@ -1,9 +1,9 @@
 function createGame(player1, hour, player2){
     return`
     <li>
-    <img src=".times/icon-${player1}.png" alt="bandeira do ${player1}"/>
+    <img src="img/icon-${player1}.png" alt="bandeira do ${player1}"/>
     <strong>${hour}</strong>
-    <img src=".times/icon-${player2}.png" alt="bandeira do ${player2}"/>
+    <img src="img/icon-${player2}.png" alt="bandeira do ${player2}"/>
     `
 }
 let delay = -0.4;
@@ -20,4 +20,14 @@ function createCard(date, day, games){
 }
 document.querySelector("#cards").innerHTML = 
 createCard("05/07", "Quarta-Feira", createGame
-("Corinthians", "21:00", "Palmeiras"))
+("Corinthians", "20:00", "Palmeiras"))+createCard("07/07", "Quarta-Feira", createGame
+("sp", "21:00", "Vasco"))+createCard("10/07", "Quarta-Feira", createGame
+("Atletico", "17:00", "Bahia"))+createCard("12/07", "Quarta-Feira", createGame
+("Bota", "20:30", "Flamengo"))+createCard("14/07", "Quarta-Feira", createGame
+("Inter", "16:30", "Santos"))+createCard("16/07", "Quarta-Feira", createGame
+("Cruzeiro", "15:00", "Gremio"))
+
+
+
+
+
